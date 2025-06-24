208931bf6dc433781eba3bf772537ded927c2ba9
Abdul Lukmana <154068062+abdullukmana@users.noreply.github.com>
2025-06-24 09:24:06 +0700

    Add files via upload

---
+++ Layout/assets/js/index.js
@@ -0,0 +1,49 @@
+function setElementPositionAndHeight({ selectorOrElement, height, top }) {
+    let elements;
+    if (typeof selectorOrElement === 'string') {
+        elements = document.querySelectorAll(selectorOrElement);
+    } else if (selectorOrElement instanceof HTMLElement) {
+        elements = [selectorOrElement];
+    } else {
+        throw new Error('Property "selectorOrElement" harus berupa string atau elemen HTML.');
+    }
+
+    elements.forEach(element => {
+        element.style.setProperty('--element-height', `${height}px`);
+        element.style.setProperty('--element-top', `${top}px`);
+    });
+}
+
+function initToggleClassHandlers() {
+    document.querySelectorAll('[data-toggle][data-target]').forEach(trigger => {
+        const className = trigger.getAttribute('data-toggle');
+        const targetSelector = trigger.getAttribute('data-target');
+        const target = document.querySelector(targetSelector);
+
+        if (target && className) {
+            trigger.addEventListener('click', () => {
+                target.classList.toggle(className);
+            });
+        }
+    });
+}
+
+initToggleClassHandlers();
+
+const header = document.querySelector('header');
+const viewportHeight = window.innerHeight;
+const headerHeight = header.offsetHeight;
+
+setElementPositionAndHeight({
+        selectorOrElement: '.sidebar',
+    height: window.innerHeight - header.offsetHeight,
+    top: header.offsetHeight
+});
+
+window.addEventListener("resize", () => {
+    setElementPositionAndHeight({
+        selectorOrElement: '.sidebar',
+        height: window.innerHeight - header.offsetHeight,
+        top: header.offsetHeight
+    });
+});
